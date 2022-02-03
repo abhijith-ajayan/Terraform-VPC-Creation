93ea3202643d17da08bff0af9caea06f86037aa2
Abhijith Ajayan <54264484+abhijith-ajayan@users.noreply.github.com>
2022-02-03 19:12:44 +0530

    bash scripts

---
+++ setup_database.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+echo "ClientAliveInterval 60" >> /etc/ssh/sshd_config
+echo "LANG=en_US.utf-8" >> /etc/environment
+echo "LC_ALL=en_US.utf-8" >> /etc/environment
+sudo hostnamectl set-hostname db-server
+
+sudo service sshd restart
+
